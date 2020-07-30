af7ba5e91d86b67381be87f811051c7661f527ae
nelson-caberto <nelson-caberto@users.noreply.github.com>
2020-07-29 19:43:58 -0500

    Added 3.1 Solution 11 Ben

---
+++ 01-Lesson-Plans/03-Python/1/Activities/11-Ins_Loops/Solved/LoopDeeLoop.Ben.py
@@ -0,0 +1,13 @@
+myList = ['Ben', 'steve', 'dave']
+for x in range(2):
+	print(x)
+for x in range(7,10):
+	print(x)
+name = "ben"
+for i in name:
+	print(i)
+for n in myList:
+	print(n)
+for n in myList:
+	for letter in n:
+		print(letter)
