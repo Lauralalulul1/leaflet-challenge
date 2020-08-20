e93a5d971e77ee424c5d5bfaed6eb67751c1e0e1
nelson-caberto <nelson-caberto@users.noreply.github.com>
2020-08-19 20:22:28 -0500

    Added 6.2 Solution 04 Ben2

---
+++ 01-Lesson-Plans/06-Python-APIs/2/Activities/04-Stu_Burundi/Solved/Stu_Burundi.Ben2.py
@@ -0,0 +1,13 @@
+cities = ['topeka,kansas', 'kansas city,kansas', 'lawrence,kansas','london']
+temps = []
+for city in cities:
+    # Build query URL and request your results in Celsius
+    query_url = f"{url}appid={api_key}&q={city}&units={units}"
+    # Get weather data
+    weather_response = requests.get(query_url)
+    weather_json = weather_response.json()
+    temperature = weather_json["main"]["temp"]
+    temps.append(temperature)
+    print(f"The temperature in {city} is {temperature}")
+weather_dict = { "city": cities, "temp": temps}
+weather_dict
